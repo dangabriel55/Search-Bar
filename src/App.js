41734abcd91921957f95de9cf7b46c2cf002cb91
dangabriel55 <cena_re_ra_dane@yahoo.com>
2022-10-10 17:15:55 +0300

    Comit Primul

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
+import { Users } from './users';
+ 
 function App() {
+  console.log(Users);
+  const [query, setQuery] = useState("");
+  console.log(Users.filter(user=>user.first_name.toLowerCase().includes("fe")));
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input onChange={(e) => setQuery(e.target.value)} type="text" placeholder='Searcccchs,...' className='search'/>
+      <ul className='list'>
+        {Users.filter(user=>user.first_name.toLowerCase().includes(query)).map((user) => (
+          <li key={user.id} className='listItem'>{user.first_name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
